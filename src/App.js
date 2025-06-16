48c8fb92817b831e94d3df481f7bff77e420b9cd
Warren Parsons <warren.parsons@ulta.com>
2025-06-16 12:07:26 -0400

    Rewrite app to use Axios for JSON

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
@@ -6,15 +7,9 @@ function App() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch posts");
-        }
-        return response.json();
-      })
-      .then((data) => setPosts(data))
-      .catch((err) => setError(err.message));
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => setPosts(response.data))
+      .catch(err => setError(err.message));
   }, []);
 
   return (
